bf1928d7325d2dbddeafd88ec41a5eedff9da565
Kashish Ahuja <kashish.ahuja-IT-2K19-24@iips.edu.in>
2022-01-03 23:02:34 +0530

    Add expense form toggle

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -38,18 +38,19 @@ const ExpenseForm = (props) => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type='text' onChange={handleTitleInput} value={inputTitle} />
+                    <input type='text' onChange={handleTitleInput} value={inputTitle} required />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" onChange={handleAmountInput} value={inputAmount} />
+                    <input type='number' min="0.01" step="0.01" onChange={handleAmountInput} value={inputAmount} required />
                 </div>
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={handleDateInput} value={inputDate} />
+                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={handleDateInput} value={inputDate} required />
                 </div>
             </div>
             <div className="new-expense__actions">
+                <button type="button" onClick={props.onCancle}>Cancle</button>
                 <button type="submit">Add Expense</button>
             </div>
         </form>
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+    const [formState,setFormState] = useState(false);
+
+    const handleFormState = () => {
+        setFormState(true);
+    }
+
+    const handleFormCancle = () => {
+        setFormState(false);
+    }
+
     const handleSubmitAddExpense = (inputExpense) => {
+        const inputExpenseData = {
+            ...inputExpense,
+            id: Math.random().toString()
+        }
+
         props.onNewExpense(inputExpense);
+        setFormState(false);
     }
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSubmitAddExpense={handleSubmitAddExpense}></ExpenseForm>
+            {!formState && <button onClick={handleFormState}>Add New Expense</button>}
+            {formState && <ExpenseForm 
+                    onSubmitAddExpense={handleSubmitAddExpense} 
+                    onCancle={handleFormCancle} 
+                />
+            }
         </div>
     )
 }
